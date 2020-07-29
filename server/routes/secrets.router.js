99853188d41c5f6b5d9557b6a7597836d2217787
“brunoreyes” <bruno619reyes@gmail.com>
2020-07-29 17:41:40 -0500

    initial commit - 1st part of base mode complete" git push origin master

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+// imputing in rejectAuthenticated as middleware, making sure
+// a user is authenticated before going ahead
+router.get('/', rejectUnauthenticated, (req, res) => {
+  console.log('req.user:', req.user);
+  pool
+    .query(
+      `SELECT * FROM "secret" WHERE
+        ${req.user.clearance_level} >= secret.secrecy_level;`
+    )//Make sure that you test out your query in SQL before implementing it above.
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log('Error making SELECT for secrets:', error);
+      res.sendStatus(500);
+    });
 });
+
+// The above is the changed version,
+// We would use a if & else statement but that has already
+// been done for us in modues>authentication-middleware
+
+// router.get('/', (req, res) => {
+//         console.log('req.user:', req.user);
+//         pool.query('SELECT * FROM "secret";')
+//             .then(results => res.send(results.rows))
+//             .catch(error => {
+//                 console.log('Error making SELECT for secrets:', error);
+//                 res.sendStatus(500);
+//             });
+
+// });
 
 module.exports = router;
